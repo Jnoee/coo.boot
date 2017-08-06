50566f3e803bbe308c76ad8b90ecf8a4fff7b3a1
jnoee <jnoee.he@gmail.com>
2017-08-07 02:01:51 +0800

    d

--- coo.boot.main/install/bin/startup.bat
+++ coo.boot.main/install/bin/startup.bat
@@ -1,4 +1,3 @@
 set APP_HOME=%cd%\..
-set CATALINA_HOME=%APP_HOME%\tomcat
 set JAVA_OPTS=-Djava.net.preferIPv4Stack=true -Xms128m -Xmx1024m
-call %CATALINA_HOME%\bin\startup.bat
+java %JAVA_OPTS% -jar *.jar
